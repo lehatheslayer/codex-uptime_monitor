41b941b0454089bceb255b0b7d4a49b0cfee6cb4
Aleksey <ziqunion95@gmail.com>
2021-10-21 00:28:54 +0300

    Rewrite Notifier.java

--- src/main/java/so/codex/monitor/service/monitor/Notifier.java
+++ src/main/java/so/codex/monitor/service/monitor/Notifier.java
@@ -1,17 +1,29 @@
 package so.codex.monitor.service.monitor;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 /**
  * Класс, который просит codex бота оповестить о проблеме в телеграм
  */
 public class Notifier {
     public static void sendAlert(String message) {
-        final Runtime rt = Runtime.getRuntime();
-
         try {
-            rt.exec("curl -X POST " + XMLParser.getWebHook() + " -d \"message=" + message + "\" -k");
-        } catch (IOException e) {
+            HttpClient client = HttpClient.newBuilder().build();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(XMLParser.getWebHook()))
+                    .POST(HttpRequest.BodyPublishers.ofString("message=" + message))
+                    .setHeader("Content-Type", "application/x-www-form-urlencoded")
+                    .build();
+
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+
+            System.out.println(response.body());
+        } catch (InterruptedException | IOException e) {
             e.printStackTrace();
         }
     }
